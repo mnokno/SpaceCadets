5200510306bf71ca548c3abef4df28d43ea88bee
mnokno <prv.nowacki@gmail.com>
2022-10-16 13:56:36 +0100

    Added option to try a list of ID's from file

--- NameScratcher/src/Main.java
+++ NameScratcher/src/Main.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Scanner;
@@ -9,10 +7,29 @@ public class Main {
     private static final String URL_BASE = "https://www.ecs.soton.ac.uk/people/";
 
     public static void main(String[] args) throws IOException {
-        // reads email id, used to construct a full url
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter email id: ");
-        String emailID = sc.nextLine();
+
+        // if test to true it will run all the names from data/testData.text instead of asking for input
+        boolean test = false;
+
+        if (!test){
+            // reads email id, used to construct a full url
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter email id: ");
+            String emailID = sc.nextLine();
+            // extracts name from email ID
+            ExtractNameFromEmailID(emailID);
+        }
+        else{
+            try (BufferedReader br = new BufferedReader(new FileReader("data/testData.txt"))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    ExtractNameFromEmailID(line);
+                }
+            }
+        }
+    }
+
+    private static void ExtractNameFromEmailID(String emailID) throws IOException {
 
         // constructs the full url, used to make URL object
         String urlString = URL_BASE + emailID;
@@ -44,7 +61,7 @@ public class Main {
             System.out.println("Name:" + lineWithName.split(":")[1]);
         }
         else{
-            System.out.println("The given email id does not line to a valid page! The page was most likely moved.");
+            System.out.println("The given email id doesn't link to a valid page! The id is invalid or the page was moved.");
         }
     }
 }
