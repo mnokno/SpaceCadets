5d832311562021c6a49dfd730cb1c8db85d45560
mnokno <prv.nowacki@gmail.com>
2022-11-02 17:03:46 +0000

    Finished Spirograph, need some gui fixes

--- Spirograph/Spirograph/MainWindow.xaml.cs
+++ Spirograph/Spirograph/MainWindow.xaml.cs
@@ -20,9 +20,90 @@ namespace Spirograph
     /// </summary>
     public partial class MainWindow : Window
     {
+        private float R;
+        private float r;
+        private float O;
+        private float P = 1000;
+        private float S = 100;
+        private float offset = 180f;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        public void PaintDot(float x, float y, int size = 1)
+        {
+            Ellipse ellipse = new Ellipse();
+            ellipse.Fill = Brushes.Black;
+            ellipse.Width = 1;
+            ellipse.Height = 1;
+            Canvas.SetTop(ellipse, y + offset);
+            Canvas.SetLeft(ellipse, x + offset);
+            canvas.Children.Add(ellipse);
+        }
+
+        public void PaintSpirographPoint(float R, float r, float O, float t)
+        {
+            float x = (R - r) * MathF.Cos(t) + O * MathF.Cos(((R-r)/r)*t);
+            float y = (R - r) * MathF.Sin(t) + O * MathF.Sin(((R - r) / r) * t);
+            PaintDot(x, y);
+        }
+
+        private void RePaintSpirograph()
+        {
+            canvas.Children.Clear();
+            for (int i = 0; i < P; i++)
+            {
+                PaintSpirographPoint(R, r, O, i / S);
+            }
+        }
+
+        private void txtBoxR_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (float.TryParse(txtBoxR.Text, out R))
+            {
+                RePaintSpirograph();
+            }
+        }
+
+        private void txtBoxr_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+            if (float.TryParse(txtBoxr.Text, out r))
+            {
+                RePaintSpirograph();
+            }
+        }
+
+        private void txtBoxO_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (float.TryParse(txtBoxO.Text, out O))
+            {
+                RePaintSpirograph();
+            }
+        }
+
+        private void txtBoxS_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (float.TryParse(txtBoxS.Text, out S))
+            {
+                RePaintSpirograph();
+            }
+        }
+
+        private void txtBoxP_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (float.TryParse(txtBoxP.Text, out P))
+            {
+                RePaintSpirograph();
+            }
+        }
+
+        private void Grid_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            offset = (float)canvas.Width / 2f;
+        }
+
+
     }
 }
